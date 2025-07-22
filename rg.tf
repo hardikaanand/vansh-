5e86868d750154124c262f3dfdcd35e83679e9bd
hardikaanand <hardikaanand36@gmail.com>
2025-07-22 16:42:51 +0530

    storage added

--- rg.tf
+++ rg.tf
@@ -17,3 +17,15 @@ resource "azurerm_resource_group" "devopsinsider" {
   name     = "devops-rg"
   location = "West Europe"
 }
+
+resource "azurerm_storage_account" "devopsinsider" {
+  name                     = "devopsinsiderstorage"
+  resource_group_name      = azurerm_resource_group.devopsinsider.name
+  location                 = azurerm_resource_group.devopsinsider.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "DevOps Insider"
+  }
+}
